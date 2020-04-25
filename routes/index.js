03585e4a44d33a49044f66f0c8cc475f884c425d
Jonathan See <jtsee@student.unimelb.edu.au>
2020-04-25 21:24:50 +1000

    redo home page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-//var views = require('./views');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Lecture5 Node&Express' });
+router.get('/', function(req, res) {
+    res.send("<H1>THIS SHOULD BE A PAGE</H1>");
 });
 
 module.exports = router;
