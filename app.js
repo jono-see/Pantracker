698f2ab4eaad22a977142d8a07635ef366d36368
Jonathan See <jtsee@student.unimelb.edu.au>
2020-04-25 22:11:11 +1000

    redo home page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*const express = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 
 
@@ -29,21 +29,21 @@ app.use("/index", router);
 // start app and listen for incoming requests on port
 app.listen(process.env.PORT || 3000, () => {
   console.log("The library app is running!");
-});*/
+});
 
 
-var express = require('express');
+/*var express = require('express');
 var path = require('path');
 
 
-/* router is also known as a mini-app
+ router is also known as a mini-app
 With routers you can modularize your code more easily. You can use routers as:
 1. Basic Routes: Home, About
 2. Route Middleware to log requests to the console
 3. Route with Parameters
 4. Route Middleware for Parameters to validate specific parameters
 5. Validates a parameter passed to a certain route
-*/
+
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 
@@ -53,16 +53,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-/*
+
 Middleware functions:
 Something you would do before passing the request to handlers.
 https://expressjs.com/en/guide/writing-middleware.html
-*/
+
 
 // app.use() is intended for binding middleware to your application.
 // The path is a "mount" or "prefix" path and limits the middleware
 // to only apply to any paths requested that begin with it. Like:
-// app.all(/^\/.*/, function (req, res) { res.send('Hello');
+// app.all(/^\/., function (req, res) { res.send('Hello');
 // });
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
@@ -73,4 +73,5 @@ app.get('/', function(req,res){
 
 
 module.exports = app;
+*/
 
