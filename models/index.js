3e397c97475e6ed17b67bcb7f36c3ea2f7d885b4
Jonathan See <jtsee@student.unimelb.edu.au>
2020-04-28 13:41:17 +1000

    add mongodb

---
+++ models/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+CONNECTION_STRING = "mongodb+srv://jono:<password>@cluster0-ek7nd.mongodb.net/test?retryWrites=true&w=majority";
+MONGO_URL = CONNECTION_STRING.replace("<password>",process.env.MONGO_PASSWORD);
+
+console.log(MONGO_URL);
+
+
+
+mongoose.connect(MONGO_URL || "mongodb://localhost/info30005", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    dbName: "Supermarkets"
+});
+
+const db = mongoose.connection;
+db.on("error", err => {
+    console.error(err);
+    process.exit(1);
+});
+db.once("open", async () => {
+    console.log("Mongo connection started on " + db.host + ":" + db.port);
+});
