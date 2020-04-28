ff33590f9f78f85e19872c5ddf6b9f01cb54976b
Jonathan See <jtsee@student.unimelb.edu.au>
2020-04-28 14:14:33 +1000

    edit home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lecture5 Node&Express' });
+  res.render('index', { title: 'Pantracker' });
 });
 
 module.exports = router;
