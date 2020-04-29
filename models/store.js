e8447d7db13f2a0047b12acd06d89e26f3c2dcc8
shenhblack <59543281+shenhblack@users.noreply.github.com>
2020-04-29 12:53:49 +1000

    Add files via upload

---
+++ models/store.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        "id":1,
+        "name":"Woolworths - Bourke St Metro",
+        "lat":-37.815699,
+        "long":144.955957,
+        "address":"600 Bourke St, Melbourne VIC 3000"
+    },
+    {
+        "id":2,
+        "name":"Woolworths - Yarraville Metro",
+        "lat":-37.816340,
+        "long":144.891670,
+        "address":"4-6 Anderson St, Yarraville VIC 3013"
+    },
+    {
+        "id":3,
+        "name":"Woolworths - Hampton",
+        "lat":-37.936874,
+        "long":145.001989,
+        "address":"355-363 Hampton St, Hampton VIC 3188"
+    },
+    {
+        "id":4,
+        "name":"Coles - Northland",
+        "lat":-37.737971,
+        "long":145.029545,
+        "address":"2/50 Murray Rd, Preston VIC 3072"
+    },
+    {
+        "id":5,
+        "name":"Coles - Middle Camberwell",
+        "lat":-37.833146,
+        "long":145.077529,
+        "address":"751 Riversdale Road, Camberwell VIC 3124"
+    },
+    {
+        "id":6,
+        "name":"Coles - Malvern",
+        "lat":-37.859913,
+        "long":145.029068,
+        "address":"164-178 Glenferrie Rd, Malvern VIC 3144"
+    }
+];
