ab78aaa920d044e7088f430dcfd3bf7648d2e6a4
shenhblack <shenblack@live.com>
2020-05-01 15:07:24 +1000

    cleaning up files

--- app.js
+++ app.js
@@ -40,10 +40,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use("/stores", storeRouter);
 
-app.get('/', function(req,res){
-  res.send("Hello World");
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
