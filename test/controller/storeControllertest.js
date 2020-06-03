a24dae438793bf43954f1c655da002dc4d0059c4
Jonathan See <jtsee@student.unimelb.edu.au>
2020-06-03 22:15:22 +1000

    add testing

---
+++ test/controller/storeControllertest.js
@@ -0,0 +1,48 @@
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+var should = require('chai').should();
+var stores = require('../../models/stores.js');
+var storeController = require('../../controller/storeController');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+let server = require('../../app.js');
+
+
+describe('testPostcode', function() {
+
+    chai.use(chaiHttp);
+    // I just want to remind that using chai is easier to read
+    describe('getValidPostcode', function() {
+            // Test to get all students record
+        it("should return valid stores", (done) => {
+            chai.request(server)
+                .get('/stores/postcode/3000')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    //res.body.should.be.a('object');
+                    done();
+                })
+        })
+        it("should return 1 valid store", (done) => {
+            chai.request(server)
+                .get('/stores/1')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    //res.body.should.be.a('object');
+                    done();
+                })
+        })
+
+        it("Postcode should return invalid since not in VIC", (done) => {
+            chai.request(server)
+                .get('/stores/search/4000')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    //res.body.should.be.a('object');
+                    done();
+                })
+        })
+    });
+});
